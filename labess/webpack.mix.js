2726ba3e00879bd30d7f5f6417259bfdacc2ff2e
tom <top9tomy@gmail.com>
2019-04-03 14:22:58 +0200

    Template
    integration template

--- labess/webpack.mix.js
+++ labess/webpack.mix.js
@@ -12,4 +12,16 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .sass('resources/sass/_contact.scss', 'public/css')
+   .sass('resources/sass/_footer.scss', 'public/css')
+   .sass('resources/sass/_global.scss', 'public/css')
+   .sass('resources/sass/_masthead.scss', 'public/css')
+   .sass('resources/sass/_mixins.scss', 'public/css')
+   .sass('resources/sass/_navbar.scss', 'public/css')
+   .sass('resources/sass/_portofolio.scss', 'public/css')
+   .sass('resources/sass/_services.scss', 'public/css')
+   .sass('resources/sass/_team.scss', 'public/css')
+   .sass('resources/sass/_timeline.scss', 'public/css')
+   .sass('resources/sass/_variables.scss', 'public/css')
+   .sass('resources/sass/agency.scss', 'public/css');
