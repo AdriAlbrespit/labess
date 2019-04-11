d8336613930ce2812515f7b0269d9f9562580904
Sarah <oursofsarah@hotmail.fr>
2019-04-11 17:13:06 +0200

    new logo
    integration nouvelle image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,9 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">LABESS</a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+            <img class="img-fluid" src="img/LABESSLOGO.png" alt="">
+          </a></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
