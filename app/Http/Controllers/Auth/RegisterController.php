6f740aefebd367d73d449bcd83aeea66c057b432
Solange <solange.bahegne@gmail.com>
2019-04-12 10:04:56 +0200

    complément 2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,10 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'adress' => ['required', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'postalcode' => ['required', 'string', 'max:5'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -65,6 +69,10 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'surname' => $data['surname'],
+            'adress' => $data['adress'],
+            'city' => $data['city'],
+            'postalcode' => $data['postalcode'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
